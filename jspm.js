147077fa1ecfdc40b37f3bc53ddcb31381ba66d6
Max Peterson <computmaxer@gmail.com>
2014-07-25 13:48:11 -0500

    Add ability to specify a custom jspm_packages path to the bundle command.

--- jspm.js
+++ jspm.js
@@ -193,8 +193,9 @@ if (require.main !== module)
     break;
 
     case 'bundle':
-      var options = readOptions(args, ['--inject']);
+      var options = readOptions(args, ['--inject'], ['--packages']);
       var inject = !!options.inject;
+      var packagesPath = options.packages;
       var bArgs = options.args;
 
       if (bArgs.length < 2) {
@@ -215,7 +216,7 @@ if (require.main !== module)
           expression = bArgs.splice(1, bArgs.length - 2).join(' ');
           fileName = bArgs[bArgs.length - 1];
         }
-        bundle.bundle(expression, fileName, inject);
+        bundle.bundle(expression, fileName, inject, packagesPath);
       }
     break;
 
